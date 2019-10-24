d76583acb482d818b71813c65a1c7cc52d5452fa
Juan Chango <jchango@udlanet.ec>
2019-10-24 18:41:43 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Destiny Roulette 
+
+A simple roulette game :godmode: coded in javascript.
+It uses `Howler` in order to play the sounds.
---
+++ configuraciones/configuracion_preguntas.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var options = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"];
+var num_opciones = [5, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3, 4, 4, 4, 4, 4, 4];
+var tiempo = [30,180,90,90,60,30,180,90,120,60,60,120,90,30,90,120,120,120,90,180];
+var respuestas = [2, 1, 2, 1, 4, 3, 4, 2, 1, 2, 3, 2, 1, 1, 3, 1, 3, 4, 3, 4];
